5314d7cf571ad0695727beff1c21987eaa97c3df
Shashi Bhattad <shashi@Shashis-MBP.fios-router.home>
2020-02-01 18:09:17 -0500

    changed deply.sh 1

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,1 @@
 docker build -t sha01/multi-client:latest -t sha01/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sha01/multi-server:latest -t sha01/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sha01/multi-worker:latest -t sha01/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push sha01/multi-client:latest
-docker push sha01/multi-server:latest
-docker push sha01/multi-worker:latest
-docker push sha01/multi-client:$SHA
-docker push sha01/multi-server:$SHA
-docker push sha01/multi-worker:$SHA
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sha01/multi-server:$SHA
-kubectl set image deployments/client-deployment server=sha01/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=sha01/multi-worker:$SHA
